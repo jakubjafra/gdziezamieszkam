d53613f89328ad1a3bc95e488aafc3d55dadde0b
khronedev <jakub.jafra@gmail.com>
2016-04-21 19:10:00 +0200

    routing change & hide offer quality

--- packages/frontend-base/config/routing.js
+++ packages/frontend-base/config/routing.js
@@ -3,9 +3,13 @@ Router.configure({
 });
 
 Router.route('/', function(){
+    this.redirect("offersList")
+});
+
+Router.route('/mieszkania/poznan/', function(){
     this.render("offersList");
 }, {name: 'offersList'});
 
-Router.route('/mine', function(){
+Router.route('/mieszkania/poznan/moje', function(){
     this.render("offersMine");
 }, {name: 'offersMine'});
